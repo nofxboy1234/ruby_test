c908211c9321fdd8a72c547567bdbf78b5c8a62c
Dylan Palmboom <nofxboy1234@gmail.com>
2022-09-30 12:04:56 +0200

    Create bubble_sort
    Working with 1 pass through

---
+++ script.rb
@@ -0,0 +1,14 @@
+require 'pry-byebug'
+
+def isogram?(string)
+  original_length = string.length
+  string_array = string.downcase.split
+
+  binding.pry
+
+  unique_length = string_array.uniq.length
+  tester = true
+  original_length == unique_length
+end
+
+p isogram?('Odin')
--- test.rb
+++ test.rb
@@ -1,100 +1,271 @@
-my_hash = {
-  'a random word' => 'ahoy',
-  "Dorothy's math test score" => 94,
-  'an array' => [1, 2, 3],
-  'an empty hash within a hash' => {
-  },
-}
+# require 'pry-byebug'
 
-my_hash = Hash.new
-p my_hash
+# friends = %w[Sharon Leo Leila Brian Arun]
 
-hash = { 9 => 'nine', :six => 6 }
-p hash
+# invited_list = []
+# friends.each { |friend| invited_list.push(friend) if friend != 'Brian' }
+# p invited_list
 
-shoes = { 'summer' => 'sandals', 'winter' => 'boots' }
-p shoes['summer']
-p shoes['hiking']
-# p shoes.fetch('hiking')
-p shoes.fetch('hiking', 'hiking boots')
+# p friends.select { |friend| friend != 'Brian' }
 
-shoes['fall'] = 'sneakers'
-p shoes
+# responses = {
+#   'Sharon' => 'yes',
+#   'Leo' => 'no',
+#   'Leila' => 'no',
+#   'Arun' => 'yes',
+# }
+# p responses.select { |person, response| response == 'yes' }
 
-shoes['summer'] = 'flip-flops'
-p shoes
+# my_numbers = [5, 6, 7, 8]
 
-shoes.delete('summer')
-p shoes
+# sum = 0
+# my_numbers.each { |number| sum += number }
+# p sum
 
-books = { 'Infinite Jest' => 'David Foster', 'Into the Wild' => 'Jon Krakauer' }
-p books.keys
-p books.values
+# p my_numbers.reduce(1000) { |sum, number| sum + number }
 
-hash1 = { 'a' => 100, 'b' => 200 }
-hash2 = { 'b' => 254, 'c' => 300 }
-p hash1.merge(hash2)
+# votes = [
+#   "Bob's Dirty Burger Shack",
+#   "St. Mark's Bistro",
+#   "Bob's Dirty Burger Shack",
+# ]
+# tally =
+#   votes.reduce(Hash.new(0)) do |result, vote|
+#     # binding.pry
+#     result[vote] += 1
+#     result
+#   end
+# p tally
 
-american_cars = { chevrolet: 'Corvette', ford: 'Mustang', dodge: 'Ram' }
-japanese_cars = { honda: 'Accord', toyota: 'Corolla', nissan: 'Altima' }
-p american_cars[:ford]
-p japanese_cars[:honda]
+# def invited_friends(friends)
+#   friends.select { |friend| friend != 'Brian' }
+# end
 
-person = { name: 'bob', height: '6 ft', weight: '160 lbs', hair: 'brown' }
-person.each { |key, value| puts "Bob's #{key} is #{value}" }
+# p friends
+# p invited_friends(friends)
 
-def greeting(name, options = {})
-  if options.empty?
-    puts "Hi, my name is #{name}"
-  else
-    puts "Hi, my name is #{name} and I'm #{options[:age]}" +
-           " years old and I live in #{options[:city]}."
+# array = %w[a b c]
+# result =
+#   array.map do |string|
+#     puts string
+#     string.upcase
+#   end
+# p result
+
+# h = { bacon: 'protein', apple: 'fruit' }
+# p h.map { |k, v| [k, v.to_sym] }.to_h
+
+# h = { bacon: 'protein', apple: 'fruit' }
+# p h.map { |pair| pair }
+
+# friends = %w[Sharon Leo Leila Brian Arun]
+# p friends.each { |friend| friend.upcase }
+# p friends.map { |friend| friend.upcase }
+
+# array = %w[a b c]
+# p array.map.with_index { |ch, idx| [ch, idx] }
+
+# h = { bacon: 'protein', apple: 'fruit' }
+# p h
+# p h.map { |k, v| [k, v] }.to_h
+
+# h = { bacon: 'protein', apple: 'fruit' }
+# p h
+# p h.map { |k, v| v }
+
+# a = %w[protein fruit]
+# p a
+# p a.map { |v| v }
+
+# a = %w[protein fruit]
+# p a
+# p a.map.with_index(5) { |v, i| [v, i] }
+
+# p a = %w[11 21 5].map(&:to_i)
+
+# a = [1, 2, 3, 4, 5]
+
+# p a.map { |n| n * 2 }
+
+# p a.map do |n|
+#   n * 2
+# end
+
+# p a.map
+
+# p (a.map do |n| n * 2 end)
+
+# x = a.map { |n| n * 2 }
+# p x
+
+# y =
+#   a.map do |n|
+#     # puts n
+#     n * 2
+#   end
+# p y
+
+#######################
+# a = [1, 2, 3, 4, 5]
+
+# p a.map { |n| n * 2 }
+# p (a.map { |n| n * 2 })
+
+# p a.map do |n|
+#   n * 2
+# end
+# p (a.map) do |n|
+#   n * 2
+# end
+# p (a.map do |n| n * 2 end)
+#######################
+
+# x = [1, 2, 3].reduce(0) { |a, v| a + v }
+# p x
+
+# x = [1, 2, 3].sum
+# p x
+
+# x = [1, 2, 3].reduce(0) { |a, v| a + 5 * v }
+# p x
+
+# x = [1, 2, 3].map { |v| v * 2 }
+# p x
+
+# def map(list, &function)
+#   list.reduce([]) { |a, v| a.push function.call(v) }
+# end
+# binding.pry
+# p map([1, 2, 3]) { |v| v * 2 }
+
+############################
+# p [1, 2, 3].select { |v| v.even? }
+
+# def select(list, &function)
+#   list.reduce([]) do |a, v|
+#     a.push(v) if function.call(v)
+#     a
+#   end
+# end
+# p select([1, 2, 3]) { |v| v.even? }
+############################
+
+############################
+# p [1, 2, 3].find { |v| v.even? }
+
+# def find(list, &function)
+#   list.reduce(nil) { |_, v| break v if function.call(v) }
+# end
+# p find([1, 2, 3]) { |v| v.even? }
+############################
+############################
+# p [1, 2, 3].count
+# p [1, 2, 3].tally
+# p [1, 2, 3].count(2)
+
+# p [1, 2, 3].count { |v| v.even? }
+# p [1, 2, 3].count { |v| v < 3 }
+
+# # tally_by
+# p [1, 2, 3].map { |v| v.even? }.tally
+
+# def tally_by(list, &function)
+#   init = Hash.new(0)
+
+#   list.reduce(init) do |a, v|
+#     key = function.call(v)
+
+#     a[key] += 1
+#     a
+#   end
+# end
+# p tally_by([1, 2, 3]) { |v| v.even? }
+############################
+############################
+# def select_map(list, &function)
+#   list.reduce([]) do |a, v|
+#     result = function.call(v)
+
+#     a.push(result) if result
+#     a
+#   end
+# end
+
+# p select_map([1, 2, 3, 4]) { |v| v.even? }
+# p select_map([1, 2, 3, 4]) { |v| v < 4 }
+
+############################
+
+# numbers = [5, 6, 7, 8]
+# element = 6
+# result = false
+
+# numbers.each { |number| result = true if number == element }
+
+# p result
+
+# element = 3
+# result = false
+
+# numbers.each { |number| result = true if number == element }
+
+# p result
+
+# p numbers.include?(6)
+####################################
+# friends = %w[Sharon Leo Leila Brian Arun]
+
+# invited_list = friends.select { |friend| friend != 'Brian' }
+
+# p invited_list.include?('Brian')
+####################################
+# numbers = [21, 42, 303, 499, 550, 811]
+
+# p numbers.any? { |number| number > 500 }
+
+# h = { a: 19, b: 2, c: 9 }
+# h.each { |pair| p pair }
+# p h.any? { |pair| pair == [:a, 1] }
+# p h.any? { |k, v| v == 1 }
+
+# fruits = %w[apple banana strawberry pineapple]
+# p fruits.all? { |fruit| fruit.length > 3 }
+# p fruits.all? { |fruit| fruit.length > 6 }
+
+# h = {}
+# p h.all? { |k, v| k == ';sdfsfds' }
+
+# fruits = %w[apple banana strawberry pineapplesssssssssssssssssss]
+
+# p fruits.none? { |fruit| fruit.length > 10 }
+
+# def caesar_cipher(str, shift_factor)
+#   str.split('').map { |letter| (letter.ord + shift_factor).chr }.join
+# end
+
+# puts caesar_cipher('What a string!', 5)
+##########################################3
+# number of elements n - 1
+
+def bubble_sort(arr)
+  arr.each_with_index do |num, i|
+    if i < arr.length - 1
+      if num > arr[i + 1]
+        tmp = arr[i + 1]
+        arr[i + 1] = num
+        arr[i] = tmp
+      end
+    end
   end
+  arr
 end
 
-greeting('Bob')
-greeting('Bob', { age: 62, city: 'New York City' })
-greeting('Bob', age: 62, city: 'New York City')
+# p bubble_sort([4, 2, 78, 2, 0, 2])
+p bubble_sort([3, 2, 9, 6, 5])
 
-z = { 'height' => '6 ft' }
-p z
-puts ({ 9 => '99 ft' })
-x = { 'height' => '17 ft' }
-puts x
-
-name_and_age = { Bob: 42, Steve: 31 }
-p name_and_age.key?(:Steve)
-p name_and_age.select { |k, v| k == :Bob }
-p name_and_age.fetch(:Steve)
-p name_and_age.fetch(:Larry, "Larry isn't in this hash")
-p name_and_age.to_a
-p name_and_age.keys
-p name_and_age.values
-
-p name_and_age.keys.each { |k| puts k }
-
-family = {
-  uncles: %w[bob joe steve],
-  sisters: %w[jane jill beth],
-  brothers: %w[frank rob david],
-  aunts: %w[mary sally susan],
-}
-
-immediate_family = family.select { |k, v| k == :sisters || k == :brothers }
-arr = immediate_family.values.flatten
-p arr
-
-h1 = { a: 1, b: 2 }
-h2 = { b: 4, c: 8 }
-p h1.merge(h2)
-p h1
-
-h1.merge!(h2)
-p h1
-
-h1.keys.each { |k| puts k }
-h1.each_key { |k| puts k }
-h1.values.each { |v| puts v }
-h1.each_value { |v| puts v }
-
-h1.each { |k, v| puts "#{k} - #{v}" }
+# [4, 2, 78, 2, 0, 2]
+# [2, 4, 78, 2, 0, 2]
+# [2, 4, 78, 2, 0, 2]
+# [2, 4, 2, 78, 0, 2]
+# [2, 4, 2, 0, 78, 2]
+# [2, 4, 2, 0, 2, 78]
